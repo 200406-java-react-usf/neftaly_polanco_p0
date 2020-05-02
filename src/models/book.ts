a0a9352b717ca73b66cc1528bd0228945959ed2f
neftaly polanco <polanconeftaly@gmail.com>
2020-05-01 23:23:00 -0400

    modified book.ts

--- src/models/book.ts
+++ src/models/book.ts
@@ -4,18 +4,14 @@ export class Book {
     genres: string;
     authorId: number;
     inStock: number;
-    readers: string;
-    publisher: string;
-    pubDate: Date;    
+    buyers: string;   
 
-    constructor(id: number, title: string, writerId: number, genres: string, status: number, readers: string, pub: string, pd: Date) {
+    constructor(id: number, title: string, writerId: number, genres: string, status: number, buyers: string, pub: string, pd: Date) {
         this.id = id;
         this.title = title;
         this.authorId = writerId;
         this.genres = genres;
         this.inStock = status;
-        this.readers = readers;
-        this.publisher = pub;
-        this.pubDate = pd;
+        this.buyers = readers;        
     }
 }
