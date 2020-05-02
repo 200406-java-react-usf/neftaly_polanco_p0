249157e8855846c9c1cb2a5e0fcd97576b491c01
neftaly polanco <polanconeftaly@gmail.com>
2020-05-01 23:10:29 -0400

    created schemas.ts

---
+++ src/util/schemas.ts
@@ -0,0 +1,22 @@
+export interface EmployeeSchema {
+    id: number,
+    username: string,
+    password: string,
+    first_name: string,
+    last_name: string,
+    birth_date: Date,
+    hire_date: Date,
+    email: string,
+    role_name: string
+}
+
+export interface BookSchema {
+
+}
+
+export interface TransactionSchema {
+    
+}
+
+export interface AuthorSchema {
+}
