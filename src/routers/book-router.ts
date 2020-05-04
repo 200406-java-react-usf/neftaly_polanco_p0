8c11302e39cf106d6cf6b9b3747e8bec810c9c86
neftaly polanco <polanconeftaly@gmail.com>
2020-05-03 23:28:01 -0400

    created book router

---
+++ src/routers/book-router.ts
@@ -0,0 +1,45 @@
+import url from 'url';
+import express from 'express';
+import AppConfig from '../config/app';
+import { isEmptyObject } from '../util/validator';
+import { ParsedUrlQuery } from 'querystring';
+import { adminGuard } from '../middleware/auth-middleware';
+
+export const BookRouter = express.Router();
+
+const bookService = AppConfig.bookService;
+
+BookRouter.get('', async (req, resp) => {
+
+    try {
+            let payload = await bookService.getAllBooks();
+            resp.status(200).json(payload);
+        
+    } catch (e) {
+        resp.status(e.statusCode).json(e);
+    }
+
+});
+
+BookRouter.get('/:id', async (req, resp) => {
+    const id = +req.params.id;
+    try {
+        let payload = await bookService.getBookById(id);
+        return resp.status(200).json(payload);
+    } catch (e) {
+        return resp.status(e.statusCode).json(e);
+    }
+});
+
+BookRouter.post('', async (req, resp) => {
+
+    console.log('POST REQUEST RECEIVED AT /books');
+    console.log(req.body);
+    try {
+        let newBook = await bookService.addNewBook(req.body);
+        return resp.status(201).json(newBook);
+    } catch (e) {
+        return resp.status(e.statusCode).json(e);
+    }
+
+});
