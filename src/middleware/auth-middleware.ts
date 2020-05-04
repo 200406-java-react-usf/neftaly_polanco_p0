123151fcd21d0ffb45a958f4aa0df1364b44ec08
neftaly polanco <polanconeftaly@gmail.com>
2020-05-04 17:54:23 -0400

    minor edit to parameter names in auth middleware

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
 import { AuthenticationError, AuthorizationError } from "../errors/errors";
 
-export const adminGuard = (request: Request, response: Response, next) => {
-    if(!request.session.principal) {
-        response.status(401).json(new AuthenticationError('No session found! Please provide valid login information.'));
-    } else if(request.session.principal.role === 'Admin') {
+export const adminGuard = (req: Request, resp: Response, next) => {
+
+    if(!req.session.principal) {
+        resp.status(401).json(new AuthenticationError('No session found! Please provide valid login information.'));
+    } else if(req.session.principal.role === 'Admin') {
         next();
     } else {
-        response.status(403).json(new AuthorizationError());
+        resp.status(403).json(new AuthorizationError());
     }
+   
 }
