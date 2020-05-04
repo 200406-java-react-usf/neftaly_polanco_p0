b593da8cf66fc516ba6d00afeb26ba28f76e0814
neftaly polanco <polanconeftaly@gmail.com>
2020-05-03 23:18:37 -0400

    added schemas for book, author and customer

--- src/util/schemas.ts
+++ src/util/schemas.ts
@@ -10,3 +10,36 @@ export interface EmployeeSchema {
     email: string,
     role_name: string
 }
+
+export interface BookSchema {
+    id: number;
+    title: string;
+    genres: string;
+    authors: string[];
+    stock: number;
+    buyers: string;
+    price: number;
+    transactionId: number;
+}
+
+export interface AuthorSchema {
+    id: number,
+    first_name: string,
+    last_name: string,
+    pen_name: string,
+    bookswritten: string[]
+}
+
+export interface CustomerSchema {
+    id: number;
+    firstName: string;
+    lastName: string;
+	birthdate: Date;
+    phone: string;
+    email: string;
+	address: string;
+	city: string;
+	state: string;
+	zipCode: number;
+    booksLog: string;
+}
