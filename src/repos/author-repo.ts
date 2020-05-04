f0f2a9274c7cf7b2c2edfbf00a31f325a10ce4b5
neftaly polanco <polanconeftaly@gmail.com>
2020-05-03 22:02:50 -0400

    created book-repo and author-repo

---
+++ src/repos/author-repo.ts
@@ -0,0 +1,114 @@
+import { Author } from '../models/author';
+import { CrudRepository } from './crud-repo';
+import { 
+    InternalServerError
+} from '../errors/errors';
+import { PoolClient } from 'pg';
+import { connectionPool } from '..';
+import { mapAuthorResultSet } from '../util/result-set-mapper';
+
+export class AuthorRepository implements CrudRepository<Author> {
+
+    baseQuery = `
+        select * from Authors
+    `;
+    // get all authors
+    async getAll(): Promise<Author[]> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery}`;
+            let rs = await client.query(sql); // rs = ResultSet
+            return rs.rows.map(mapAuthorResultSet);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+
+    //get author by id
+    async getById(id: number): Promise<Author> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery} where au.id = $1`;
+            let rs = await client.query(sql, [id]);
+            return mapAuthorResultSet(rs.rows[0]);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    }
+
+    //add a new author
+    async save(newAuthor: Author): Promise<Author> {
+            
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+           
+            let sql = `
+                insert into Authors (first_name, last_name, pen_name) 
+                values ($1, $2, $3) returning id
+            `;
+            let rs = await client.query(sql, [newAuthor.firstName, newAuthor.lastName, newAuthor.penName]);
+            
+            newAuthor.id = rs.rows[0].id;
+            
+            return newAuthor;
+
+        } catch (e) {
+            console.log(e);
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+
+    //update an existing author
+
+    async update(updatedAuthor: Author): Promise<boolean> {
+        
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `update Authors where au.id = $1 values `;
+            let rs = await client.query(sql, []);
+            return true;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+    
+    //delete author
+    async deleteById(id: number): Promise<boolean> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `delete from Authors where id = $1`;
+            await client.query(sql, []);
+            return true;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+
+    }
+
+}
---
+++ src/repos/book-repo.ts
@@ -0,0 +1,143 @@
+// import book model
+import { Book } from '../models/book';
+//import crud repo
+import { CrudRepository } from './crud-repo';
+
+import {
+    InternalServerError
+} from '../errors/errors';
+//import connection
+import { PoolClient } from 'pg';
+import { connectionPool } from '..';
+//import map result set
+import { mapBookResultSet } from '../util/result-set-mapper';
+
+//create book repository
+export class BookRepository implements CrudRepository<Book> {
+
+    baseQuery = `
+        select *
+
+        from Books b
+        join Copies_Sold cs
+        on b.id = cs.book_id
+
+        join Transactions t
+        on cs.transaction_id = t.id
+
+        join Written_By wb
+        on b.id = wb.book_id
+
+        join Authors au
+        on wb.author_id = au.id
+
+        join Book_Genres bg
+        on b.id = bg.book_id
+
+        join Genres g
+        on bg.genre_id = g.id
+    `;
+
+    //get all books
+    async getAll(): Promise<Book[]> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery}`;
+            let rs = await client.query(sql); // rs = ResultSet
+            return rs.rows.map(mapBookResultSet);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+
+    //get book by id
+    async getById(id: number): Promise<Book> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `${this.baseQuery} where b.id = $1`;
+            let rs = await client.query(sql, [id]);
+            return mapBookResultSet(rs.rows[0]);
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    }
+
+    // add new book
+    async save(newBook: Book): Promise<Book> {
+            
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+
+            let bookAuthors = {...newBook.authors}
+           
+            let sql = `
+                insert into Books (title, stock, price) 
+                values ($1, $2, $3) returning id
+                insert into Book_Genres values ($4, $5) returning id
+                insert into Written_By values ($1, $6)
+            `;
+            let rs = await client.query(sql, [newBook.title, newBook.stock, newBook.price, newBook.genres, newBook.authors]);
+            
+            newBook.id = rs.rows[0].id;
+            
+            return newBook;
+
+        } catch (e) {
+            console.log(e);
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+
+    //update book work in progress
+    async update(updatedBook: Book): Promise<boolean> {
+        
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = ``;
+            let rs = await client.query(sql, []);
+            return true;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+    
+    }
+
+    //delete a book
+    async deleteById(id: number): Promise<boolean> {
+
+        let client: PoolClient;
+
+        try {
+            client = await connectionPool.connect();
+            let sql = `delete from Books where b.id = $1`;
+            await client.query(sql, []);
+            return true;
+        } catch (e) {
+            throw new InternalServerError();
+        } finally {
+            client && client.release();
+        }
+
+    }
+
+}
