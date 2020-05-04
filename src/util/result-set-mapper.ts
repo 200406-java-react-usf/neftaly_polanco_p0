5569187679a0497cefc52707ccaee5659ae5c07e
neftaly polanco <polanconeftaly@gmail.com>
2020-05-03 23:21:08 -0400

    added result set mappers for book, author and customer

--- src/util/result-set-mapper.ts
+++ src/util/result-set-mapper.ts
@@ -23,3 +23,57 @@ export function mapEmployeeResultSet(resultSet: EmployeeSchema): Employee {
         resultSet.role_name
     );
 }
+
+export function mapBookResultSet(resultSet: BookSchema): Book {
+    
+    if (!resultSet) {
+        return {} as Book;
+    }
+
+    return new Book(
+        resultSet.id,
+        resultSet.title,  
+        resultSet.stock, 
+        resultSet.price,  
+        resultSet.genres,
+        resultSet.authors,
+        resultSet.buyers,        
+        resultSet.transactionId,
+    );
+}
+
+export function mapAuthorResultSet(resultSet: AuthorSchema): Author {
+    
+    if (!resultSet) {
+        return {} as Author;
+    }
+
+    return new Author(
+        resultSet.id,
+        resultSet.first_name,  
+        resultSet.last_name, 
+        resultSet.pen_name,
+        resultSet.bookswritten
+    );
+}
+
+export function mapCustomerResultSet(resultSet: CustomerSchema): Customer {
+
+    if(!resultSet) {
+        return {} as Customer;
+    }
+
+    return new Customer(
+        resultSet.id,
+        resultSet.firstName,
+        resultSet.lastName,
+        resultSet.birthdate,
+        resultSet.phone,
+        resultSet.email,
+        resultSet.address,
+        resultSet.city,
+        resultSet.state,
+        resultSet.zipCode,
+        resultSet.booksLog
+    );
+}
