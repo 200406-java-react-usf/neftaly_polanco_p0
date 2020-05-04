08e6a9357025ce3fa4782ba9ed7514f6d113333c
neftaly polanco <polanconeftaly@gmail.com>
2020-05-03 20:05:21 -0400

    added getEmployeeById and getEmployeeByUniqueKey to employee-service

--- src/services/employee-service.ts
+++ src/services/employee-service.ts
@@ -1,6 +1,7 @@
 import { Employee } from "../models/employee";
 import { EmployeeRepository } from "../repos/employee-repo";
-import { ResourceNotFoundError } from "../errors/errors";
+import { ResourceNotFoundError, BadRequestError } from "../errors/errors";
+import { isValidId, isEmptyObject, isPropertyOf } from "../util/validator";
 
 
 
@@ -18,6 +19,55 @@ export class EmployeeService {
         return emps.map(this.removePassword);
     }
 
+    async getEmployeeById(id: number): Promise<Employee> {
+        if (!isValidId(id)) {
+            throw new BadRequestError();
+        }
+
+        let employee = await this.empRepo.getById(id);
+
+        if(isEmptyObject(employee)) {
+            throw new ResourceNotFoundError();
+        }
+
+        return  this.removePassword(employee);
+    }
+
+    async getEmployeeByUniqueKey(queryObj: any): Promise<Employee> {
+
+        try {
+            let queryKeys = Object.keys(queryObj);
+
+            if(!queryKeys.every(key => isPropertyOf(key, Employee))) {
+                throw new BadRequestError();
+            }
+
+            //searching by only one key (at least for now)
+            let key = queryKeys[0];
+            let val = queryObj[key];
+
+            //reuse getById logic if given key is id
+            if (key === 'id') {
+                return await this.getEmployeeById(+val);
+            }
+
+            // throw error if key is value is not valid
+            if(!isValidId(val)) {
+                throw new BadRequestError();
+            }
+
+            let emp = await this.empRepo.getEmployeeByUniqueKey(key, val);
+            
+            if(isEmptyObject(emp)) {
+                throw new ResourceNotFoundError();
+            }
+
+            return this.removePassword(emp);
+
+    } catch (e) {
+        throw e;
+    }
+    }
     private removePassword(employee: Employee): Employee {
         if (!employee|| !employee.password) return employee;
         let emp = {...employee};
