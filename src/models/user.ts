e38ee3fb137da0f66edd1e3793cf8d32d2d7f35e
neftaly polanco <polanconeftaly@gmail.com>
2020-05-05 23:15:38 -0400

    changed model name from employee to user

---
+++ src/models/user.ts
@@ -0,0 +1,25 @@
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    birthdate: Date;
+    hireDate: Date;
+    phone: string;
+    email: string;
+    role_name: string;
+    
+    constructor(id: number, un: string, pw: string, fn: string, ln: string, bd: Date, hd: Date, phone: string, email: string, role: string) {
+        this.id = id;
+        this.username = un;
+        this.password = pw;
+        this.firstName = fn;
+        this.lastName = ln;
+        this.birthdate = bd;
+        this.hireDate = hd;
+        this.phone = phone;
+        this.email = email;
+        this.role_name = role;
+    }
+}
